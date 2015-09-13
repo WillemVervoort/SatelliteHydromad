91f28b086ac81521eddaec657fedc8c0361389f2
WillemVervoort <willemvervoort@gmail.com>
2015-09-13 19:58:36 +1000

    Final version of MODIS.transform()

--- 20150910_MODIS_to_Timeseries.R
+++ 20150910_MODIS_to_Timeseries.R
@@ -2,18 +2,19 @@
 # Transform MODIS Satellite data to 8 day time series for modelling
 # this should be a function
 # Dir where MODIS data is stored (extracted with MODISTOOLS), so asc files
-# This function might have to be adjusted if you are working with tif files
-
-getwd()
-xy.loc <- read.csv("CorinPoints.csv")
+# This function can also work with tif files
 
 # Function to check for leap years
 leap.fun <- function(start.y,end.y) {
+  # Utility function to deal with leap years
+  # This function generates the "5" and "6" values
+  # needed to find the dates of all the MODIS tile observations
   # find the first year
   year.b <- as.numeric(substr(start.y,1,4))
   year.e <- as.numeric(substr(end.y,1,4))
   # calculate when the next leap year will occur
   leap.count <-  (year.b/4 - floor(year.b/4))*4
+  # Generate the series of "5" and "6"
   if (leap.count == 0) {
     extra.y <- (year.e-year.b)/4 - floor((year.e-year.b)/4) 
     leap1 <- rep(c(6,rep(5,3)),floor((year.e-year.b)/4))
@@ -33,189 +34,134 @@ leap.fun <- function(start.y,end.y) {
 
   }
   # for testing
-  return(list(leap.out=leap,leap.count.out=leap.count))
-         #return(leap)
+  #return(list(leap.out=leap,leap.count.out=leap.count))
+  return(leap)
 }
 
 # test
 #test <- leap.fun(start.y="2000-01-01",end.y="2008")
 
+# MODIS transform function
 MODIS.transform <- function(MODISdir="MODIS",patt=".asc",
-                            start="2000-01-01", end="2008-12-31"
-                            xy.loc)
-  #patt can also be "*tif" will get to this later
-  
-  if (patt == ".asc") {
+                            start="2000-01-01", end="2008-12-31",
+                            xy.loc=NULL, merge.FUN=mean) 
+{
+  # this function transforms the MODIS data downloaded with MODIS tools
+  # to a proper zoo timeseries rather than Julian dates
+  # it also does catchment averaging
+  # MODISdir is the directory where the MODIS downloads can be found
+  # patt is the pattern to look for (Either tif or "asc")
+  # start and end are the start and end dates
+  # xy.loc are the xy locations of the downloaded sites 
+  # (only needed if patt = ".tif")
+  # This is two column data.frame with lon and lat
+  # merge.FUN is the function to use to merge the data
+  # if merge.FUN is FALSE, than all the series are returned
+  #packages
+  require(maptools,quietly=T)
+  require(raster,quietly=T)
+  require(rgdal,quietly=T)
+
+
   # This first looks at the list of files
   x1 <- list.files(path=MODISdir,
-                   pattern=patt)
+                 pattern=patt)
 
-  n <- nrow(read.csv(paste(MODISdir,x1[1],sep="/"),header=F))
+  #patt can also be "*tif" will get to this later
+  if (patt == ".asc") {
+    # for the asc files, each files stores all the values in time for 1 location
+    n <- nrow(read.csv(paste(MODISdir,x1[1],sep="/"),header=F))
+    # Create storage for the data
+    Store <- data.frame(Year = numeric(length=n),
+                        Jdate = numeric(length=n),
+                        ET = numeric(length=n))
+    # list to store the different locations
+    Store1 <- list()
+    
+    for (i in 1:length(x1)) {
+      Mdata <- read.csv(paste(MODISdir,x1[i],sep="/"),header=F)
+      Store[,1] <- as.numeric(substr(Mdata[1:n,8],2,5))
+      Store[,2] <- as.numeric(substr(Mdata[1:n,8],6,8))
+      Store[,3] <- Mdata[1:n,11]/10 ##  0.1 scaling factor (see MODIS read_me)
+      Store1[[i]] <- Store
+    }
+  } # end if patt = tif
+  # in case the files are tif files and need to be first rastered
+  if (patt==".tif") {
+    # for the tif files, each individual file has all locations for one time
+    # define locations as spatial points
+    loc <- SpatialPoints(cbind(xy.loc$lon, xy.loc$lat), proj4string=CRS("+proj=longlat"))
+    EandN <- spTransform(loc, CRS("+init=epsg:28355"))
+    
+    n <- nrow(xy.loc)
+    # run through the files and extract values
+    Store <- data.frame(Year = numeric(length=n),
+                        Jdate = numeric(length=n),
+                        ET = numeric(length=n))
+    # list to store the different times
+    Store1 <- list()
 
-  Store <- data.frame(Year = numeric(length=n),
-                    Jdate = numeric(length=n),
-                    ET = numeric(length=n))
-  Store1 <- list()
+    for(i in 1:length(x1)){
+      #i=1
+#      test <- readGDAL(paste(MODISdir,x1[i],sep="/"))
+      Mdata <- raster(paste(MODISdir,x1[i],sep="/"))
+      
+      Store[,1] <- as.numeric(substr(strsplit(x1[i],"[.]")[[1]][2],2,5))
+      Store[,2] <- as.numeric(substr(strsplit(x1[i],"[.]")[[1]][2],6,8))
+      Store[,3] <- extract(Mdata,EandN)
+      Store1[[1]] <- Store
+    }
+  } # end if patt = tif
+    
 
-  for (i in 1:length(x1)) {
-    Mdata <- read.csv(paste(MODISdir,x1[i],sep="/"),header=F)
-    Store[1:n,1] <- as.numeric(substr(Mdata[1:n,8],2,5))
-    Store[1:n,2] <- as.numeric(substr(Mdata[1:n,8],6,8))
-    Store[1:n,3] <- Mdata[1:n,11]/10 ##  0.1 scaling factor (see MODIS read_me)
-    Store1[[i]] <- Store
-  }
-
-
+  # rbind to one data.frame  
   ETunlist <- do.call(rbind,Store1)
-  # # check distribution
+  # # you can check distribution
   # hist(test$ET, main="Histogram of raw ET values across all pixels", 
   #      xlab="8 daily sum ET (mm)")
 
-  ET.mean <- aggregate(ETunlist[,3],list(Jdate=ETunlist$Jdate, Year=ETunlist$Year), 
-                     mean,na.rm=T)
-  
+  # Now aggregate across the catchment
+  ET.mean <- aggregate(ETunlist[,3],list(Jdate=ETunlist$Jdate, 
+                        Year=ETunlist$Year), merge.FUN,na.rm=T)
+  # Julian Days
   Jdays <- ET.mean$Jdate
-  
+  # work out number of days between observations (not always 8!)
   days.cor <- c(diff(as.numeric(Jdays)),5)
-  # Now we need to replace the -360 values
-  
-  # run leap.fun
+  # Now we need to replace the -360 values at the end of the year
+  # run leap.fun to account for leap years
   leap <- leap.fun(start,end)
   # correct the -360 values
   days.cor[days.cor==-360] <- leap
   # check
   #days.cor
-  
-  # GOT TO HERE ON 20150910
-  
+    
   ## get dates for each 8-day tile
   for (i in 1:(length(days.cor)-1)) {
     if (i ==1) {
-      dates.tiles <- seq.Date(as.Date("2000-01-01"),
+      dates.tiles <- seq.Date(as.Date(start),
                               by=days.cor[i],length.out=2)
     } else {
       dates.tiles <- c(dates.tiles,
                        (seq.Date(as.Date(dates.tiles[length(dates.tiles)]),
-                                 by=days.cor[i],length.out=2))[2])
+                                   by=days.cor[i],length.out=2))[2])
     }
   }
   # you could make a plot to check
-  plot(as.Date(dates.tiles),Store1[[1]][,3],type="l")
-  
-  data.m <- do.call(cbind,Store1) 
-  data.m2 <- data.m[,-grep("Jdate",names(data.m))]
-  data.m3 <- data.m2[,-grep("Year",names(data.m2))]
-  data.ET <- data.m3
-  data.ET$MFRSGL <- data.ET$MFRAA.ET
-  data.ET$Dates <- dates.tiles
-  data.ET$Year <- data.m[,1]
-  
-  
-  days.cor <- c(rep(8,45),6,rep(c(rep(8,45),5),3),rep(8,45),6,
-              rep(c(rep(8,45),5),3),rep(8,45),6)
-  # length(days.cor)
-  # # should equal
-  # nrow(z.mean)
-  ET.mean.cor <- ET.mean$x/days.cor
-  #z.mean.cor is ET/days.cor so it is the ET for one day, rather than the sum of 8 days - however, it is still at 8-day intervals
-
-  plot(ET.mean$x, type="p", ylab="ET (mm)",ylim=c(0,16)) 
-  # this is the 8 day sum of ET
-points(ET.mean.cor,lty=2,col="red") #this is the 8 day sum divided by 8 (or 5/6)
-legend("topleft",c("8 day sum of ET", "daily values"),
-       col=c(1,"red"), pch=1)
-
-# reset z.mean for rest of script
-ET.mean.raw <- ET.mean
-ET.mean <- ET.mean.cor
-
-
-## get dates for each 8-day tile
-for (i in 1:length(days.cor)) {
-  if (i == 1) {
-    dates.tiles <- seq.Date(as.Date("2000-01-01"),
-                            by=days.cor[i]-1,length.out=2)
-  } else {
-    dates.tiles <- c(dates.tiles,
-                     (seq.Date(as.Date(dates.tiles[length(dates.tiles)]),
-                               by=days.cor[i],length.out=2))[2])
-  }
+  #plot(as.Date(dates.tiles),ET.mean[,3],type="l")
+  return(data.frame(Dates=dates.tiles,ET=ET.mean$x))
 }
 
-modis.days <- ET.mean.raw ## Julian days the modis image was taken
-dates <- as.character(seq.Date(as.Date("2000-01-01"),
-                               to=as.Date("2008-12-31"),by=1))
-Jdates <- c(1:length(dates[grep("2000",dates)]),
-            1:length(dates[grep("2001",dates)]),
-            1:length(dates[grep("2002",dates)]), 
-            1:length(dates[grep("2003",dates)]),
-            1:length(dates[grep("2004",dates)]),
-            1:length(dates[grep("2005",dates)]),
-            1:length(dates[grep("2006",dates)]),
-            1:length(dates[grep("2007",dates)]),
-            1:length(dates[grep("2008",dates)]))
-full.days <- 1:length(Jdates)
-# create a dataframe to combine
-JDates2 <- data.frame(count=full.days,Julian=Jdates,dates=dates,
-                      year=as.numeric(substr(dates,1,4)))
 
-# find matching Modis days
-modis.days2 <- JDates2[(JDates2$Julian %in% modis.days$Jdate & 
-                          JDates2$year %in% modis.days$Year),] 
+# Example
+Today <- format(Sys.Date(),"%Y%m%d")
+basedir <- "C:/Users/rver4657/ownCloud/working/SatelliteHydromad"
+setwd(basedir)
+# read in file with xy locations (or create a data.frame)
+xy.loc <- read.csv("CorinPoints.csv")
 
-## Loess smooth
-smooth.interpET <- loess.smooth(modis.days2$count, ET.mean, span = 0.04, 
-                                degree = 2, evaluation = length(Jdates))$y
-# smooth.interp is the daily, lumped, aET value, with length of 4383.
+# run function with defaults
+ET.out <- MODIS.transform()
 
-
-
-## Plot smooth  and linear spline
-plot(full.days,smooth.interpET, type = "l", xaxt="n", lwd=2,
-     ylim = c(0,3), ylab = "MODIS ET (mm)", xlab = "Time")       
-m.days <- as.Date(paste(modis.days$Jdate,modis.days$Year,sep="-"),"%j-%Y")
-points(modis.days2$dates,ET.mean,col="red")
-ticks <- cumsum(rep(365, times = 10))-365
-axis(1, at = ticks, labels = 2000:2009) 
-legend("topleft",c("smooth MODIS ET", "MODIS 8 day values"),lwd=c(1,NA),
-       col=c(1,"red"), pch=c(NA,1))
-
-
-# THIS BELOW SHOULD DO 8 DAY EXTRACTION
-
-
-Jdays <- Store1[[1]]$Jdate
-
-days.cor <- c(diff(as.numeric(Jdays)),5)
-# Now we need to replace the -360 values, 2000 is a leap year
-# there are 14 years alltogether
-leap <- c(rep(c(6,rep(5,3)),3),6,5)
-days.cor[days.cor==-360] <- leap
-# check
-days.cor
-
-## get dates for each 8-day tile
-for (i in 1:(length(days.cor)-1)) {
-  if (i ==1) {
-    dates.tiles <- seq.Date(as.Date("2000-01-01"),
-                            by=days.cor[i],length.out=2)
-  } else {
-    dates.tiles <- c(dates.tiles,
-                     (seq.Date(as.Date(dates.tiles[length(dates.tiles)]),
-                               by=days.cor[i],length.out=2))[2])
-  }
-}
-# you could make a plot to check
-plot(as.Date(dates.tiles),Store1[[1]][,3],type="l")
-
-data.m <- do.call(cbind,Store1) 
-data.m2 <- data.m[,-grep("Jdate",names(data.m))]
-data.m3 <- data.m2[,-grep("Year",names(data.m2))]
-data.ET <- data.m3
-data.ET$MFRSGL <- data.ET$MFRAA.ET
-data.ET$Dates <- dates.tiles
-data.ET$Year <- data.m[,1]
-
-## write to file
-write.csv(data.ET, file=paste("x:/vervoort/research/cotter/MODISET/",today,"_modisETa.csv",sep=""),
-          row.names=F)
+# write file away
+write.csv(ET.out,paste(Today,"AverageCorinCatchmentET.csv",sep="_"),row.names=F)
+  
