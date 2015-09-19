f4e74c59a011bc0ad50f89940e96a78eae47833d
Joseph Guillaume <josephguillaume@gmail.com>
2015-09-19 11:40:24 +0300

    Clean ExtractPoints

--- 20150910_ExtractPointsFunction.R
+++ 20150910_ExtractPointsFunction.R
@@ -8,57 +8,51 @@
 # a shape file of the area
 # a test image from MODIS or a different satellite
 
+#Install packages if missing
+for(pkg in c("raster","maptools","rgeos","rgdal"))
+  if(!require(pkg,character.only=TRUE)) install.packages(pkg)
+library(raster)
+library(maptools)
+library(rgeos)
+library(rgdal)
 
 # You don't need this function if you have the lat and long of your locations
-Extract.points <- function(wdir=getwd(), shape_name="corin_pro", test_image,
-                           proj = "+proj=longlat +ellps=WGS84") {
-    # wdir is the working directory, required
-    # shape_name is the base name for the shapefile
-    # test_image is the name of a test MODIS satellite image of the area
-    # proj is the projection of the shape file
-    # Following packages are required
-    # make sure all packages are updated
-    # this needs an error message? using try?
-    try({require(raster)
-    require(maptools)
-    require(rgeos)
-    require(rgdal)})
-
-    # Reading the shape file of the catchment
-    shape <- readShapePoly(paste(wdir,paste(shape_name,"shp",sep="."),sep="/"))
-
-    # setting up the projection of the shapefile
-    crs(shape) <- proj
-
-    # reading in the test MODIS file
-    modis <- raster(paste(wdir,test_image,sep="/")) 
-#     image(modis)
-#     plot(shape, add = T)
-
-    # Now clipping to the catchment boundary
-    modis.clip <- crop(modis, extent(shape)) 
-    modis.clip <- mask(modis.clip, shape)
-
-    image(modis.clip)
-    plot(shape, add = T)
-    # Now extracting the points
-    p <- rasterToPoints(modis.clip) # it contains the cell values as well
-
-    #plotting 
-    points(p)
+Extract.points <- function(wdir=getwd(), catchment_file, modis_eg_file,show.plot=TRUE) {
+  # wdir is the working directory, required
+  # catchment_file is the base name for the catchment shapefile
+  # modis_eg_file is the name of a test MODIS satellite image of the area
   
-    # only taking the x(lon) and y(lat) values
-    return(data.frame(lon = p[,1], lat = p[,2]))
+  # Reading the shape file of the catchment
+  shape <- readOGR(wdir,catchment_file)
+  
+  # reading in the test MODIS file
+  modis <- raster(paste(wdir,modis_eg_file,sep="/")) 
+  if(show.plot){
+    image(modis)
+    lines(shape)
+  }
+  
+  # Now clipping to the catchment boundary
+  modis.clip <- crop(modis, extent(shape)) 
+  modis.clip <- mask(modis.clip, shape)
+
+  # Now extracting the points
+  p <- rasterToPoints(modis.clip) # it contains the cell values as well
+    
+  if(show.plot){
+    image(modis.clip)
+    lines(shape)
+    points(p)
+  }
+  
+  # only taking the x(lon) and y(lat) values
+  return(data.frame(lon = p[,1], lat = p[,2]))
 }
 
-# Example
-# Use data in folder Shapefiles_testdata
-basedir <- "C:/Users/rver4657/ownCloud/working/SatelliteHydromad"
-
-
-Points <- Extract.points(wdir=paste(basedir,"/Shapefiles_Testdata",sep=""),
-               shape_name="corin_pro",test_image="test1",
-               proj = "+proj=longlat +ellps=WGS84")
-head(Points)
-# write away for later use
-# write.csv(Points,paste(basedir,"Corinpoints.csv",sep="/"))
+# # Example
+# # Use data in folder Shapefiles_testdata
+#  Points <- Extract.points(wdir="Shapefiles_Testdata",
+#                 catchment_file="corin_pro",modis_eg_file="test1")
+# head(Points)
+# # write away for later use
+# # write.csv(Points,"Corinpoints.csv")
