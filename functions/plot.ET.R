ad30fda840c9392f80805e667ef49c688315ba9b
Joseph Guillaume <josephguillaume@gmail.com>
2015-09-19 11:57:35 +0300

    Fix warning for plot.ET

--- functions/plot.ET.R
+++ functions/plot.ET.R
@@ -8,9 +8,9 @@ plot.ET <- function(caldata,ModelFit) {
        lwd=4, lty=2,ylim=c(0,max(caldata$aET)+1), 
        main = "ETfun using Viney")
   plot.time <- unique(caldata$et.period)
-  lines(zoo(aggregate(ModelFit$U$ET,
-                      list(date=caldata$et.period),sum),
-            order.by=plot.time))
+  totals=aggregate(coredata(ModelFit$U$ET),
+                     list(date=coredata(caldata$et.period)),sum)
+  lines(zoo(totals[,2],order.by=totals[,1]))
   legend("topleft",c("MODIS ET", "Predicted aET"),
          lwd=c(3,1),col=c("red",1),lty=c(2,1))
 }
